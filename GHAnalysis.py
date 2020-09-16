d8169a0800d4b16a51042ca8fdea8c1b780095d6
5588yy <1114190948@qq.com>
2020-09-16 21:34:01 +0800

    code stander

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -3,9 +3,9 @@ import sys,getopt,json,os
 #批量读取json文件 初始化
 def read_json(path):
     filelist = os.listdir(path)#读取文件列表
-    f2=open('data.json','w',encoding='utf-8')
+    f2 = open('data.json','w',encoding='utf-8')
     for file in filelist:#读json文件
-        pathname=path+'\\'+file
+        pathname = path+'\\'+file
         with open(pathname,encoding='utf-8') as f:
             for line in f:
                 f2.write(line)#写入data.json
@@ -13,17 +13,17 @@ def read_json(path):
 
 #获得答案
 def caculate_ans(data,username,repo,event):
-    ans=0
+    ans = 0
     for da in data:
-        if not len(username)==0:#判断有无username
-            if not username==da['actor']['login']:
+        if not len(username) == 0:#判断有无username
+            if not username == da['actor']['login']:
                 continue
             else:
                 pass
         else:
             pass
-        if not len(repo)==0:#判断有无repo
-            if not repo==da['repo']['name']:
+        if not len(repo) == 0:#判断有无repo
+            if not repo == da['repo']['name']:
                 continue
             else:
                 pass
@@ -31,26 +31,26 @@ def caculate_ans(data,username,repo,event):
             pass
         #判断项目
         if da['type'] == event:
-                ans=ans+1
+                ans = ans+1
         else:
             pass
     return ans
 
 if __name__ == '__main__':
     #参数
-    data=[]
-    username=''
-    repo=''
-    event=''
+    data = []
+    username = ''
+    repo = ''
+    event = ''
     #命令参数
-    opt,arv= getopt.getopt(sys.argv[1:],'i:u:r:e:',['user=','repo=','event=','init='])
+    opt,arv = getopt.getopt(sys.argv[1:] , 'i:u:r:e:',['user=','repo=','event=','init='])
     #初始化
     if opt in ("-i" , "--init"):
         read_json(opt[0][1])
         print(0)
         exit()
     else:#数据读取
-        with open("data.json", encoding='utf-8') as f:
+        with open("data.json", encoding = 'utf-8') as f:
             for line in f:
                 data.append(json.loads(line))
     #查询信息
